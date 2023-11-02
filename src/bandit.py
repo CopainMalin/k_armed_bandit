cdcfe1624afe6ac8f5b7fb875322e4657d4ead21
CopainMalin <bgautier3800@gmail.com>
2023-11-02 12:49:35 +0100

    adding len method to bandit

--- src/bandit.py
+++ src/bandit.py
@@ -10,6 +10,9 @@ class Bandit:
     def __repr__(self) -> str:
         return "[" + ", ".join(map(str, self.__arms)) + "]"
 
+    def __len__(self) -> int:
+        return len(self.__arms)
+
     def get_reward(self, arm_number: int) -> float:
         return self.__arms[arm_number].generate_reward()
 
--- tests/test_bandit.py
+++ tests/test_bandit.py
@@ -3,14 +3,26 @@ from numpy.random import normal
 from src.bandit import Bandit
 import pytest
 
+
 @pytest.fixture
 def generate_bandit() -> Bandit:
-    return Bandit([GaussianArm(0, 1), *[GaussianArm(loc=random_number, scale=1)
-                    for random_number in normal(loc=0, scale=5, size=9)]])
+    return Bandit(
+        [
+            GaussianArm(0, 1),
+            *[
+                GaussianArm(loc=random_number, scale=1)
+                for random_number in normal(loc=0, scale=5, size=9)
+            ],
+        ]
+    )
 
 
 def test_get_reward(generate_bandit):
     assert -3 <= generate_bandit.get_reward(0) <= 3
+
+
+def test_len(generate_bandit):
+    assert len(generate_bandit) == 10
 
 
 def test_get_qstar(generate_bandit):
